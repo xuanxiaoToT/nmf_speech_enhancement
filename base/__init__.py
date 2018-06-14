9c12a510c5a678867190afa3da3eabcdf933f574
Meepoljd <jiandong.liu93@gmail.com>
2018-06-14 22:29:47 +0800

    rafactor: new_base
    重构base，更改base的初始化时机，在加入enhancer中获取参数。

--- base/__init__.py
+++ base/__init__.py
@@ -3,11 +3,3 @@
 # @Author  : Liu jiandong
 # @FileName: __init__.py
 # @Blog    ：http://meepoljd.com
-
-import os
-import librosa
-import numpy as np
-
-from nmf import nmf
-from config import rank, sample_rate, fft_len, common_space_err
-from .utils import get_spec, get_common_space, remove_common_space
--- base/static_dict.py
+++ base/static_dict.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
 # @Time    : 6/14/18 10:35 AM
 # @Author  : Liu jiandong
-# @FileName: ns_static_dict.py
+# @FileName: static_dict.py
 # @Blog    ：http://meepoljd.com
 
-from . import *
+import os
+import librosa
+import numpy as np
+
+import config
+import nmf
 
 
 class StaticDict:
 
-    def __init__(self, K=rank):
-        self._rank = K
+    def __init__(self, noise_path, speech_path, rank=config.rank):
+        self._rank = rank
         self._noise_dict = None
         self._speech_dict = None
         self._total_dict = None
+        self._n_fft = 0
+        self._win = ''
+        self._hop_len = 0
+        self._noise_path = noise_path
+        self._speech_path = speech_path
 
     @property
     def rank(self):
@@ -31,26 +41,57 @@ class StaticDict:
     def speech_dict(self):
         return self._speech_dict
 
-    @staticmethod
-    def _build_dict(path):
+    @property
+    def n_fft(self):
+        return self._n_fft
+
+    @n_fft.setter
+    def n_fft(self, value):
+        self._n_fft = value
+
+    @property
+    def hop_len(self):
+        return self._hop_len
+
+    @hop_len.setter
+    def hop_len(self, value):
+        self._hop_len = value
+
+    @property
+    def win(self):
+        return self._win
+
+    @win.setter
+    def win(self, value):
+        self._win = value
+
+    def _gen_spec(self, path):
         _spec = None
         _list = os.listdir(path)
         for file in _list:
             file_name = os.path.join(path, file)
-            signal, _ = librosa.load(file_name, sr=sample_rate)
-            spec = np.abs(get_spec(signal))
+            signal, _ = librosa.load(file_name, sr=config.sample_rate)
+            spec = librosa.stft(signal, n_fft=self._n_fft, hop_length=self._hop_len, window=self._win)
+            abs_spec = np.abs(spec)
             if _spec is None:
-                _spec = spec
+                _spec = abs_spec
             else:
-                _spec = np.column_stack([_spec, spec])
+                _spec = np.column_stack([_spec, abs_spec])
         return _spec
 
-    def build_noise(self, n_path):
-        self._noise_dict = self._build_dict(n_path)
-        if self._speech_dict is not None:
+    def build_noise(self):
+        _noise_spec = self._gen_spec(self._noise_path)
+        self._noise_dict, _ = nmf.decompose(_noise_spec)
+
+    def build_speech(self):
+        _speech_spec = self._gen_spec(self._speech_path)
+        self._speech_dict, _ = nmf.decompose(_speech_spec)
+
+    def merge_dic(self):
+        if self._noise_dict is not None and self._speech_dict is not None:
             self._total_dict = np.column_stack([self._speech_dict, self._noise_dict])
 
-    def build_speech(self, s_path):
-        self._speech_dict = self._build_dict(s_path)
-        if self._noise_dict is not None:
-            self._total_dict = np.column_stack([self._speech_dict, self._noise_dict])
+    def init_dic(self):
+        self.build_noise()
+        self.build_speech()
+        self.merge_dic()
