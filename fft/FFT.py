42f3491db043a04163b93adad1707fd891738482
Meepoljd <jiandong.liu93@gmail.com>
2018-06-16 15:32:49 +0800

    fix: fix mat
    convert mat to array

--- fft/FFT.py
+++ fft/FFT.py
@@ -4,60 +4,66 @@
 # @FileName: FFT.py
 # @Blog    ：http://meepoljd.com
 
-from . import *
+
+import numpy as np
+import scipy.signal as signal
+import scipy.fftpack as fft
 
 
 class FFT:
 
-    def __init__(self, stream, sr=sample_rate, win_len=fft_len, hop=hop_rate, window='hann'):
-        self._sample_rate = sample_rate
-        self._win_len = win_len
-        self._fft_count = sr * (win_len / 1000)
-        self._hop = hop
-        self._hop_count = int(self._fft_count)
+    def __init__(self, stream, n_fft_ms, hop_ms, window='hann'):
+        self._sample_rate = stream.sample_rate
+        self._n_fft = int((n_fft_ms / 1000) * self._sample_rate)
+        self._hop = int((hop_ms / 1000) * self._sample_rate)
         self._signal_arr = np.array([])
         self._vad_arr = np.array([])
         self._stream = stream
         self._win = window
-        self._win_array = get_window(self._win, self._win_len, fftbins=False)
+        self._win_array = signal.get_window(self._win, self._n_fft, fftbins=False)
 
     @property
-    def stream(self):
-        return self._stream
-
-    @stream.setter
-    def stream(self, value):
-        self._stream = value
+    def sample_rate(self):
+        if hasattr(self, '_sample_rate'):
+            return self._sample_rate
+        else:
+            return self._stream.sample_rate
 
     @property
-    def hop_count(self):
-        return self._hop_count
+    def hop(self):
+        if hasattr(self, '_hop'):
+            return self._hop
+        else:
+            return self._stream.hop
 
     @property
     def win(self):
-        return self._win
+        if hasattr(self, '_win'):
+            return self._win
+        else:
+            return self._stream.win
 
     @property
-    def fft_count(self):
-        return self._fft_count
+    def n_fft(self):
+        if hasattr(self, '_n_fft'):
+            return self._n_fft
+        else:
+            return self._stream.n_fft
 
     def __next__(self):
-        try:
-            while len(self._signal_arr) < self._fft_count:
-                vad_flag, new_sig = self._stream.__next__()
-                self._signal_arr = np.concatenate([self._signal_arr, new_sig])
-                if vad_flag:
-                    self._vad_arr = np.concatenate([self._vad_arr, np.ones(len(new_sig))])
-                else:
-                    self._vad_arr = np.concatenate([self._vad_arr, np.zeros(len(new_sig))])
-        except StopIteration:
-            raise StopIteration
-        win_sig = self._win_array * self._signal_arr[:self._win_len]
-        res = np.fft.fft(win_sig)
-        res_flag = (np.sum(self._vad_arr[:self._win_len]) / self._win_len) > 0.5
-        self._signal_arr = self._signal_arr[self._win_len:]
-        self._vad_arr = self._vad_arr[self._win_len:]
-        return res_flag, res
+        while len(self._signal_arr) < self._n_fft:
+            vad_flag, new_sig = self._stream.__next__()
+            self._signal_arr = np.concatenate([self._signal_arr, new_sig])
+            if vad_flag:
+                self._vad_arr = np.concatenate([self._vad_arr, np.ones(len(new_sig))])
+            else:
+                self._vad_arr = np.concatenate([self._vad_arr, np.zeros(len(new_sig))])
+        win_sig = self._win_array * self._signal_arr[:self._n_fft]
+        res = fft.fft(win_sig)
+        res_flag = (np.mean(self._vad_arr[:self._n_fft])) > 0.5
+        self._signal_arr = self._signal_arr[self._n_fft:]
+        self._vad_arr = self._vad_arr[self._n_fft:]
+        return res_flag, res[:(int(1 + self._n_fft // 2))]
 
     def __iter__(self):
         return self
