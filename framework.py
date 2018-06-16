cc0f9097dc7b9ba83124d81bea59a41dc61d4ca3
Meepoljd <jiandong.liu93@gmail.com>
2018-06-16 15:28:11 +0800

    refactor: fix it to current version
    more params in builder

--- framework.py
+++ framework.py
@@ -4,17 +4,22 @@
 # @FileName: framework.py
 # @Blog    ：http://meepoljd.com
 
+import config
 from stream import StreamBuilder
 from vad import Vad
 from fft import FFT
+from base import StaticDict
+from enhancer import NmfEnhancer
 
 
 def main():
-    builder = StreamBuilder(stream_type='file', filename='data/speech/train/fadg0_sa1.wav')
+    builder = StreamBuilder(stream_type='file', filename='data/speech/train/fadg0_sa1.wav', sr=-1, duration=30)
     file_stream = builder.get_instance()
     vad_stream = Vad(file_stream)
-    fft_stream = FFT(vad_stream)
-    for res in fft_stream:
+    fft_stream = FFT(vad_stream, 32, 16)
+    dic = StaticDict('data/debug/noise', 'data/debug/speech', rank=config.rank)
+    enhancer_stream = NmfEnhancer(fft_stream, dic)
+    for res in enhancer_stream:
         print(res)
 
 
